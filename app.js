c599547fb5357e1db1b2ea81d998f45ab0634f1c
Muhammad Ismaeel <m.ismaeel.developer@gmail.com>
2025-06-16 14:48:42 +0500

    .

--- app.js
+++ app.js
@@ -32,19 +32,20 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3001',
     'https://cafe-fresco-dashboard.vercel.app',
-    'https://cafe-fresco-main.vercel.app'
+    'https://cafe-fresco-main.vercel.app',
+    'https://cafe-fresco-main-muhammaad-ismaeel-s-projects.vercel.app'
 ];
 
 app.use(cors({
-origin: function (origin, callback) {
-    console.log('Incoming origin:', origin);
-    if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-    } else {
-        console.log('Blocked origin:', origin);
-        callback(new Error('CORS Not Allowed'));
-    }
-},
+    origin: function (origin, callback) {
+        console.log('Incoming origin:', origin);
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            console.log('Blocked origin:', origin);
+            callback(new Error('CORS Not Allowed'));
+        }
+    },
 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
